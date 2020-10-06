d253f7281247d58f16eb9825f40df14e664b13e9
Jason Buckner <jason@jasonbuckner.com>
2020-10-05 21:00:32 -0700

    Restore the Example app

---
+++ Example/UITests/StaticViewsUITests.swift
@@ -0,0 +1,64 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//
+//  Documentation
+//  https://jessesquires.github.io/JSQDataSourcesKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQDataSourcesKit
+//
+//
+//  License
+//  Copyright © 2015-present Jesse Squires
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+//
+
+import XCTest
+
+final class StaticViewsUITests: XCTestCase {
+
+    private let numberOfCellsInStaticTableView: Int = 9
+    private let numberOfCellsInStaticCollectionView: Int = 10
+
+    private let staticTableViewMenuItem = XCUIApplication().tables[Identifiers.mainTableView.rawValue].cells.element(boundBy: 0)
+    private let staticCollectionViewMenuItem = XCUIApplication().tables[Identifiers.mainTableView.rawValue].cells.element(boundBy: 1)
+
+    let app = XCUIApplication()
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = true
+        app.launch()
+    }
+
+    func test_ThatStaticTableView_LoadsItsCells() {
+        // GIVEN: a static table view
+        // WHEN: we choose to present the static table view
+        staticTableViewMenuItem.tap()
+
+        // THEN: the number of cells loaded matches the number of cells expected
+        let table = app.tables[Identifiers.staticTableView.rawValue]
+        let countTableCells = countElements(ofType: .cell, inView: table) { $0.identifier }
+
+        XCTAssertEqual(countTableCells,
+                       numberOfCellsInStaticTableView,
+                       "The number of cells loaded should be the same as the number of cells expected")
+    }
+
+    func test_ThatStaticCollectionView_loadsItsCells() {
+        // GIVEN: a static collection view
+        // WHEN: we choose to present the static collection view
+        staticCollectionViewMenuItem.tap()
+
+        // THEN: the number of cells loaded matches the number of cells expected
+        let collection = app.collectionViews[Identifiers.staticCollectionView.rawValue]
+        let countCollectionCells = countElements(ofType: .cell, inView: collection) { $0.identifier }
+
+        XCTAssertEqual(countCollectionCells,
+                       numberOfCellsInStaticCollectionView,
+                       "The number of cells loaded should be the same as the number of cells expected")
+    }
+}
---
+++ Example/UITests/XCTestCase+Extensions.swift
@@ -0,0 +1,135 @@
+//
+//  Created by Jesse Squires
+//  https://www.jessesquires.com
+//
+//
+//  Documentation
+//  https://jessesquires.github.io/JSQDataSourcesKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQDataSourcesKit
+//
+//
+//  License
+//  Copyright © 2015-present Jesse Squires
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+//
+
+import XCTest
+
+typealias XCUIElementType = XCUIElement.ElementType
+
+extension XCTestCase {
+
+    /**
+     Returns to the previous screen in navigation stack if Back button is availible
+     */
+    func navigateBack() {
+        let backButton = XCUIApplication().navigationBars.buttons["JSQDataSourcesKit"]
+        backButton.tap()
+    }
+
+    /**
+     Scrolls down the current view halfscreen.
+     */
+    func scrollHalfScreenDown() {
+        scrollScreenVerticallyWithOffset(-0.8 * XCUIApplication().windows.element(boundBy: 0).frame.height / 2)
+    }
+
+    /**
+     Scrolls up the current view halfscreen.
+     */
+    func scrollHalfScreenUp() {
+        scrollScreenVerticallyWithOffset(0.8 * XCUIApplication().windows.element(boundBy: 0).frame.height / 2)
+    }
+
+    /**
+     Scrolls the current view.
+     
+     - parameter offset: Number of points that a view should be scrolled by.
+     */
+    func scrollScreenVerticallyWithOffset(_ offset: CGFloat) {
+        let mainWindow = XCUIApplication().windows.element(boundBy: 0)
+
+        // `start` is exactly the center of the main window
+        let start = mainWindow.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5))
+
+        let end = start.withOffset(CGVector(dx: 0, dy: offset))
+
+        start.press(forDuration: 0, thenDragTo: end)
+    }
+
+    /**
+     Counts elements of a specific type in `view` by repeatedly scrolling it.
+     
+     **Example**
+     
+     let foo = countElements(ofType: .Cell,
+     inView: table,
+     byUniqueIdentifier: { $0.identifier })
+     
+     - important: This method scrolls from the current position down to the end. Make sure you scroll to the top before and
+     after calling it.
+     - parameter type: A type of elements to count.
+     - parameter view: A view to count elements in.
+     - parameter indentifier: A closure that takes an element and returns some value associated with it. This value has
+     to be unique, otherwise this method will not function properly.
+     
+     - returns: The number of presented unique elements.
+     */
+    func countElements(ofType type: XCUIElementType,
+                       inView view: XCUIElement,
+                       byUniqueIdentifier identifier: (XCUIElement) -> String) -> Int {
+        var accumulator = Set<String>()
+
+        var setOfVisibleElementsIdentifiersBeforeScroll = Set<String>()
+        var setOfVisibleElementsIdentifiersAfterScroll = Set<String>()
+
+        // Repeat until scrolling makes no changes.
+        repeat {
+            setOfVisibleElementsIdentifiersAfterScroll = setOfVisibleElementsIdentifiersBeforeScroll
+
+            let currentlyVisibleElements = view.descendants(matching: type).allElementsBoundByIndex.filter { $0.isHittable }
+
+            setOfVisibleElementsIdentifiersBeforeScroll = Set(currentlyVisibleElements.map { identifier($0) })
+
+            accumulator = accumulator.union(setOfVisibleElementsIdentifiersBeforeScroll)
+
+            scrollHalfScreenDown()
+
+        } while setOfVisibleElementsIdentifiersAfterScroll != setOfVisibleElementsIdentifiersBeforeScroll
+
+        return accumulator.count
+    }
+
+    ///  Sends a tap event to hittable elements of specified type.
+    func tapOn(_ numberOfElementsToTapOn: Int,
+               hittableElementsOfType type: XCUIElementType,
+               inView view: XCUIElement) {
+        let hittableElements = view.descendants(matching: type).allElementsBoundByIndex.filter { $0.isHittable }
+
+        if hittableElements.count < numberOfElementsToTapOn {
+            return
+        }
+
+        var numberOfElementsTapped = 0
+        for element in hittableElements where numberOfElementsTapped < numberOfElementsToTapOn {
+            element.tap()
+            numberOfElementsTapped += 1
+        }
+    }
+
+    @discardableResult
+    func waitToAppearFor(element: XCUIElement, timeout: Double = 5.0) -> XCTWaiter.Result {
+        let waiter = XCTWaiter()
+        let nsPredicate = NSPredicate(format: "exists == true")
+        let expectations = [XCTNSPredicateExpectation(predicate: nsPredicate, object: element)]
+        let result = waiter.wait(for: expectations, timeout: timeout)
+
+        if result == .timedOut && element.exists && nsPredicate.evaluate(with: element) {
+            return .completed
+        }
+        return result
+    }
+}
